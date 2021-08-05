d25ff12361fc66f54f1c58f0b7b79facbbc62252
Tin Tran <trantrongtinlhp@gmail.com>
2021-08-05 18:43:39 +0800

    modify mf.sh

--- mf.sh
+++ mf.sh
@@ -5,7 +5,7 @@ do
         clear && clear;
     fi;
 	echo $k;
-	i=0;
+	i=1;
 	for f in cons/*;
 	do
 		# echo $f;
@@ -16,7 +16,9 @@ do
 			break;
 		fi;
 	done
-	python "demo/restoration_video_demo.py" "./configs/restorers/iconvsr/iconvsr_vimeo90k_bd.py" \
-        "./iconvsr_vimeo90k_bd_20210414-5f38cb34.pth" ./cons2/ ./output;
-	#mv ./cons2/* ./cons3;
+	python "demo/restoration_video_demo.py" "./configs/restorers/basicvsr/basicvsr_reds4.py" \
+        "./iter_300000.pth" ./cons2/ ./output;
+#	python "demo/restoration_video_demo.py" "./configs/restorers/tdan/tdan_vimeo90k_bix4_ft_lr5e-5_400k.py" \
+#        "./tdan_vimeo90k_bix4_20210528-739979d9.pth" ./cons2/ ./output;
+	mv ./cons2/* ./cons3;
 done
